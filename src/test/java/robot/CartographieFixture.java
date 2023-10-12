4ddd33006ff70bb0a5a99d8b79504b195f7d7194
arnalsauvage <arnaudmedina@gmail.com>
2023-10-12 16:29:05 +0200

    Correction tests cartographie

--- src/test/java/robot/CartographieFixture.java
+++ src/test/java/robot/CartographieFixture.java
@@ -2,6 +2,10 @@ package robot;
 
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
+import org.junit.Assert;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CartographieFixture {
     private MyWorld world;
@@ -17,7 +21,23 @@ public class CartographieFixture {
 
     @Then("^la carte du robot mesure (\\d+) x (\\d+) cases$")
     public void laCarteMesure(int sizeX, int sizeY) throws Throwable {
-        // TODO
+        int nombreDeLignes = world.getRobot().carte().size() - 3;
+        int nombreDeColonnes = countOccurrences(world.getRobot().carte().get(3),'\t') -3;
+        Assert.assertEquals(sizeX  , nombreDeColonnes);
+        Assert.assertEquals( sizeY , nombreDeLignes);
+    }
+
+    private static int countOccurrences(String str, char ch) {
+
+        Matcher matcher = Pattern.compile(String.valueOf(ch))
+                .matcher(str);
+
+        int counter = 0;
+        while (matcher.find()) {
+            counter++;
+        }
+
+        return counter;
     }
 
 }
